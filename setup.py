89c8583a7d29836e19a62d65473a5e8acf23fe5e
Brett <brett.whynot@gmail.com>
2025-03-17 09:53:35 +1000

    v0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="teslemetry_stream",
-    version="0.6.15",
+    version="0.7.0",
     author="Brett Adams",
     author_email="hello@teslemetry.com",
     description="Teslemetry Streaming API library for Python",
